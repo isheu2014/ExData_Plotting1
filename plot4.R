72b082bebeea490523f2c6eb318f0e9ed2526522
Iris <isheu2014@gmail.com>
2014-12-28 15:19:14 -0500

    initial commit

--- plot4.R
+++ plot4.R
@@ -5,7 +5,7 @@ png(filename = "plot4.png")
 par(mfrow=c(2,2))
 plot(data$Time, as.numeric(data$Global_active_power), type = "l",
      xlab = " ", ylab="Global Active Power (kilowatts)")
-plot(data$Time, Voltage, type = "l", xlab = "datetime", ylab="Voltage")
+plot(data$Time, data$Voltage, type = "l", xlab = "datetime", ylab="Voltage")
 plot(time, data$Sub_metering_1, type = "l", xlab = " ", ylab= "Energy sub metering")
 lines(time, data$Sub_metering_2, col = "red")
 lines(time, data$Sub_metering_3, col = "blue")
@@ -13,3 +13,4 @@ legend("topright", col=c("black", "blue", "red"),
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1)
 plot(time, data$Global_reactive_power, 
      type = "l", xlab = "datetime", ylab="Global_reactive_power")
+dev.off()
